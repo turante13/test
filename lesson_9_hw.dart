05a612531fc48d2bd094fa99f4fe97d141ecadad
Turan <turan_te@mail.ru>
2021-10-26 15:39:14 +0600

    first commit

---
+++ lesson_9_hw.dart
@@ -0,0 +1,31 @@
+void main() {
+  // List list = [3, 4, 5, 2, 5, 5, 9, 5];
+  // for (int i = 0; i < list.length; i++) {
+  //   if (list[i] == 5) {
+  //     print("yes");
+  //   } else {
+  //     print("no");
+  //   }
+  // }
+
+  int a = 2;
+  int b = -56;
+  // equal(a, b);
+  negative(a);
+}
+
+equal (a, b) {
+  if (a == b) {
+    print("true");
+  } else {
+    print("false");
+  }
+}
+
+negative (a) {
+  if (a < 0) {
+    print("true");
+  } else {
+    print("false");
+  }
+}
